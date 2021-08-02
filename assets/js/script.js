74210e768040f7ef5f7ce4990fc4a16bd488b912
Camilo Rodriguez <camilo.rod2412@gmail.com>
2021-08-02 15:10:12 -0500

    Arreglos e insertar imagenés

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,41 +4,62 @@ const getData = (api) => {
   return fetch(api)
     .then((response) => response.json())
     .then((json) => {
-      llenarDatos(json.results), paginacion(json.next, json.previous);
+      pokeData(json.results), paginacion(json.next, json.previous);
     })
     .catch((error) => {
       console.log("Error ", error);
     });
 };
 
-
-const llenarDatos = (data) => {
+const pokeData = (data) => {
   let html = "";
-  
+  document.getElementById("datosPokemon").innerHTML = "";
   data.forEach((pk) => {
-        html += '<div class="col mt-5">';
-        html += '<div class="card" style="width: 15rem;">';
-        html += '<div class="card-body">';
-        html +=  `<h5 class="card-title">${pk.name}</h5>`;
-   
-        html += '</div>';
-        html += '</div>';
-        html += '</div>';
-  
+    const URL = pk.url;
+    return fetch(URL)
+      .then((response) => response.json())
+      .then((json) => {
+        llenarDatos(json, html);
+      })
+      .catch((error) => {
+        console.log("Error ", error);
+      });
   });
+};
 
-  document.getElementById("datosPokemon").innerHTML = html;
+const llenarDatos = (data, html) => {
+  html += '<div class="col mt-5">';
+  html += '<div class="card" style="width: 15rem;">';
+  html += `<img src="${data.sprites.other.dream_world.front_default}" class="card-img-top" alt="${data.name}">`;
+  html += '<div class="card-body">';
+  html += `<h5 class="card-title">${data.name}</h5>`;
+  html += `<p class="card-text">Weight: ${data.weight}</p>`;
+  html += `<p class="card-text">Height: ${data.height}</p>`;
+
+  let tipos = data.types;
+
+  tipos.forEach((type) =>{
+     
+    html += `<p class="card-text">Type: ${type.type.name}</p>`;
+  });
+  
+  html += "</div>";
+  html += "</div>";
+  html += "</div>";
+  document.getElementById("datosPokemon").innerHTML += html;
 };
 
 const paginacion = (next, prev) => {
   let prevDisable = "";
   let nextDisable = "";
 
-  let html = `<li class="page-item ${ prev == null ? prevDisable = "disable" : prevDisable = ""}"><a class ="page-link" onclick = "getData('${prev}')">Previous</a></li> <li class="page-item ${next == null ?  nextDisable = "disable": nextDisable = ""}"><a class ="page-link" onclick = "getData('${next}')">Next</a></li>`;
- 
+  let html = `<li class="page-item ${
+    prev == null ? (prevDisable = "disable") : (prevDisable = "")
+  }"><a class ="page-link" onclick = "getData('${prev}')">Previous</a></li> <li class="page-item ${
+    next == null ? (nextDisable = "disable") : (nextDisable = "")
+  }"><a class ="page-link" onclick = "getData('${next}')">Next</a></li>`;
 
-  document.getElementById("paginacion").innerHTML=html;
-
+  document.getElementById("paginacion").innerHTML = html;
 };
 
 getData(API);
